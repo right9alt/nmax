0d82f9c8fd08af141699f8f770c8b72e600aa966
right9alt <right9alt@gmail.com>
2023-03-25 14:50:08 +0300

    add conditions if is float

--- lib/nmax/numbers_processing.rb
+++ lib/nmax/numbers_processing.rb
@@ -11,15 +11,50 @@ module Nmax
     end
 
     private
+    #Вариант когда можно находить все целые числа
+    #и хранить их в массиве и поочередно пушить в основной
+    #@storage, но если массив данных очень большой, то может
+    #отнять много памяти и проход по каждому символу
+    #может и будет чуть дольше, но скорее всего производительнее
+
+    #def fill_data
+    ## Используем регулярное выражение для поиска целых чисел
+    #  integers_regex = /\b\d+\b/
+      
+    ## Ищем все целые числа в строке и добавляем их в массив
+    #  @stream.scan(integers_regex).each do |match|
+    #    @storage.push(match.to_i)
+    #  end
+    #end
+
     def fill_data
+      #В условии говорится что необходимо выводить n
+      #самых больших целых чисел, если встречаем число
+      #15151515.9999999, то необходимо его пропустить, 
+      #
+      #Считаем число типа float если после целой части
+      #стоит . и сразу же начинается дробная часть
       number = ''
-      @stream.each_char do |symbol|
-        if /\d/.match?(symbol)
-          number += symbol
+      f_is_float = false 
+      @stream.each_char.with_index do |symbol, index|
+        flag_match = /\d/.match?(symbol)
+        if f_is_float
+          unless flag_match 
+            f_is_float = false
+          end
         else
-          next if number.empty?
-          @storage.push(number.to_i)
-          number = ''
+          if flag_match
+            number += symbol
+          else
+            if symbol == '.' && /\d/.match?(@stream[index + 1])
+              f_is_float = true
+              number = ''
+            else
+              next if number.empty?
+              @storage.push(number.to_i)
+              number = ''
+            end
+          end
         end
       end
     end
